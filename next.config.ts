8db1dcbbe07e41d1332a8a0b7c67e8e01682493d
Will Mirabella <williamjmirabella@gmail.com>
2025-06-24 17:06:25 -0400

    Fixed Routing and Styling for Github Pages

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  distDir: 'out',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
